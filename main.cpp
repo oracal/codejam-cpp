344637a34f79cc90454208b5eb99d5b8fc8fe71e
Thomas Whitton <mail@thomaswhitton.com>
2013-04-03 22:11:57 +0100

    Cleaned up headers

--- main.cpp
+++ main.cpp
@@ -1,6 +1,5 @@
 #include <fstream>
 #include <iostream>
-#include <string>
 
 void processAllCases(std::istream &is, std::ostream &os);
 void processCase(const int iteration, std::istream &is, std::ostream &os);
